55a3a301cd3f16c577dfe19f198f7b266d4e2604
gidina <georginaserraoliveras@gmail.com>
2023-12-05 09:38:46 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,15 +10,15 @@ import { IconType } from "react-icons";
 interface QuinaLink {
   to: string;
   title?: string;
-  color: string;
+  bgColor: string;
   icon?: IconType;
-  children?: ReactElement;
 }
-const QuinaLink: FC<QuinaLink> = ({ to, title, icon, color, children }) => {
+
+const QuinaLink: FC<QuinaLink> = ({ to, title, icon, bgColor }) => {
   return (
     <Link
       to={to}
-      className={`py-32 px-8 rounded-2xl text-8xl no-underline text-black flex justify-center items-center gap-8 transition-transform hover:scale-105 hover:shadow-xl bg-[${color}]`}
+      className={`py-32 px-8 rounded-2xl text-8xl no-underline text-black flex justify-center items-center gap-8 transition-transform hover:scale-105 hover:shadow-xl ${bgColor}`}
     >
       <>
         {title} {icon && React.createElement(icon, { className: "h-16 w-16" })}
@@ -34,26 +34,26 @@ const Home = () => {
         to="hollywood"
         title="Hollywood"
         icon={MdMovieCreation}
-        color="#F38181"
+        bgColor="bg-[color:var(--color-hollywood)]"
       />
       <QuinaLink
         to="explosiva"
         title="Explosiva"
         icon={FaBomb}
-        color="#FCE38A"
+        bgColor="bg-[color:var(--color-explosiva)]"
       />
-      <QuinaLink to="tongo" title="Tongo" icon={ImEvil2} color="#C5E898" />
+      <QuinaLink to="tongo" title="Tongo" icon={ImEvil2} bgColor="bg-[color:var(--color-tongo)]" />
       <QuinaLink
         to="cantada"
         title="Cantada"
         icon={PiMicrophoneStageFill}
-        color="#95E1D3"
+        bgColor="bg-[color:var(--color-cantada)]"
       />
       <QuinaLink
         to="musical"
         title="Musical"
         icon={PiMusicNotesFill}
-        color="#FFCF96"
+        bgColor="bg-[color:var(--color-musical)]"
       />
     </div>
   );
