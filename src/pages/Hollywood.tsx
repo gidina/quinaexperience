f08ea324614fd89b9b19c450aa829fd58c2a0d5c
gidina <georginaserraoliveras@gmail.com>
2023-12-13 21:19:38 +0100

    Update Hollywood.tsx

--- src/pages/Hollywood.tsx
+++ src/pages/Hollywood.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BingoDobleMusical from "../BingoDobleMusical";
-import songs from "../musical";
+import songs from "../assets/hollywood";
 import { MdMovieCreation } from "react-icons/md";
 
 const Hollywood = () => {
@@ -9,7 +9,7 @@ const Hollywood = () => {
       <h2 className="absolute right-16 top-36 text-4xl font-semibold flex gap-4">
         Quina Hollywood <MdMovieCreation />
       </h2>
-      <BingoDobleMusical songs={songs} />
+      <BingoDobleMusical songs={songs} maxNumber={70} />
     </div>
   );
 };
