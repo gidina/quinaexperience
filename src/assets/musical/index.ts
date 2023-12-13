5235e6b8178b22a5df045d7b47fa6b844d03d22f
gidina <georginaserraoliveras@gmail.com>
2023-12-13 14:36:33 +0100

    Add files via upload

--- src/assets/musical/index.ts
+++ src/assets/musical/index.ts
@@ -1,122 +1,183 @@
-import fitxer1 from "./1.wav";
-import fitxer2 from "./2.wav";
-import fitxer3 from "./3.wav";
-import fitxer4 from "./4.wav";
-import fitxer5 from "./5.wav";
-import fitxer6 from "./6.wav";
-import fitxer7 from "./7.wav";
-import fitxer8 from "./8.wav";
-import fitxer9 from "./9.wav";
-import fitxer10 from "./10.wav";
+import file1 from "./1.Bomba-King Africa.m4a";
+import file2 from "./2.Dos Hombres y un Destino.m4a";
+import file3 from "./3.Torero.m4a";
+import file4 from "./4.Ave Maria.m4a";
+import file5 from "./5.Vivo por ella.m4a";
+import file6 from "./6.Que la detengan.m4a";
+import file7 from "./7.La madre de jose.m4a";
+import file8 from "./8.Dragostean din tei.m4a";
+import file9 from "./9.Como Camaron.m4a";
+import file10 from "./10.Coti x Coti Mon DJ DJ Capde The Tyets.mp3";
+import file11 from "./11.Una Vaina Loca.m4a";
+import file12 from "./12.Cancion el Velero.m4a";
+import file13 from "./13.Antes muerta que sencilla.m4a";
+import file14 from "./14.Lavantando las manos.m4a";
+import file15 from "./15.entre poetas y presos.m4a";
+import file16 from "./16.Despacito.m4a";
+import file17 from "./17.Caminando por la vida.m4a";
+import file18 from "./18.Bailando por ahi.m4a";
+import file19 from "./19.A tu lado OT.m4a";
+import file20 from "./20.Mienteme.m4a";
+import file21 from "./21.Un beso y una flor.m4a";
+import file22 from "./22.Como te atreves a volver.m4a";
+import file23 from "./23.Ni una sola palabra.m4a";
+import file24 from "./24.Princesas.m4a";
+import file25 from "./25.nada fue un error.m4a";
+import file26 from "./26.Mi gran noche.m4a";
+import file27 from "./27.Mon amour.mp3";
+import file28 from "./28.Sofia.m4a";
+import file29 from "./29.Fiesta Pagana.m4a";
+import file30 from "./30.Sube la mano parriba.m4a";
+import file31 from "./31.Noche de estrellas.m4a";
+import file32 from "./32.Suerte (wherever whererver).m4a";
+import file33 from "./33.Highway to hell.m4a";
+import file34 from "./34.Barbie Girl.m4a";
+import file35 from "./35.It_s My life.m4a";
+import file36 from "./36.video killed the radio star.m4a";
+import file37 from "./37.basket case.m4a";
+import file38 from "./38.Don_t Stop me now.m4a";
+import file39 from "./39.Wannabe.m4a";
+import file40 from "./40.Ni tu ni nadie.m4a";
+import file41 from "./41.It_s Raining men.m4a";
+import file42 from "./42.loco.m4a";
+import file43 from "./43.Let me out.m4a";
+import file44 from "./44.sweet caaroline.mp3";
+import file45 from "./45.Sueño su boca.m4a";
+import file46 from "./46.You_re the one.m4a";
+import file47 from "./47.Libertad Nil Moliner.m4a";
+import file48 from "./48.Voglio vederti danzare.m4a";
+import file49 from "./49.corazon latino.m4a";
+import file50 from "./50.Fisica y quimica.m4a";
+import file51 from "./51.una foto en blanco y negro.m4a";
+import file52 from "./52. viva la vida coldplay.mp3";
+import file53 from "./53.La reina del pop.m4a";
+import file54 from "./54.Flying Free.m4a";
+import file55 from "./55.King George.m4a";
+import file56 from "./56.Dime.m4a";
+import file57 from "./57.Europe_s Living a celebraton.m4a";
+import file58 from "./58.Cuando Zarpa el amor.m4a";
+import file59 from "./59.Yo quiero bailar.m4a";
+import file60 from "./60.Me muero por conocerte.m4a";
+import file61 from "./56.Dime.m4a";
+import file62 from "./56.Dime.m4a";
+import file63 from "./56.Dime.m4a";
+import file64 from "./56.Dime.m4a";
+import file65 from "./56.Dime.m4a";
+import file66 from "./56.Dime.m4a";
+import file67 from "./56.Dime.m4a";
+import file68 from "./56.Dime.m4a";
+import file69 from "./56.Dime.m4a";
+import file70 from "./56.Dime.m4a";
+import file71 from "./56.Dime.m4a";
+import file72 from "./56.Dime.m4a";
+import file73 from "./56.Dime.m4a";
+import file74 from "./56.Dime.m4a";
+import file75 from "./56.Dime.m4a";
+import file76 from "./56.Dime.m4a";
+import file77 from "./56.Dime.m4a";
+import file78 from "./56.Dime.m4a";
+import file79 from "./56.Dime.m4a";
+import file80 from "./56.Dime.m4a";
+import file81 from "./56.Dime.m4a";
+import file82 from "./56.Dime.m4a";
+import file83 from "./56.Dime.m4a";
+import file84 from "./56.Dime.m4a";
+import file85 from "./56.Dime.m4a";
+import file86 from "./56.Dime.m4a";
+import file87 from "./56.Dime.m4a";
+import file88 from "./56.Dime.m4a";
+import file89 from "./56.Dime.m4a";
+import file90 from "./56.Dime.m4a";
 
 export default [
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
-
-  fitxer1,
-  fitxer2,
-  fitxer3,
-  fitxer4,
-  fitxer5,
-  fitxer6,
-  fitxer7,
-  fitxer8,
-  fitxer9,
-  fitxer10,
+  { title: "Bomba", singer: "King Africa", src: file1 },
+  { title: "Dos hombres y un destino", singer: "Bustamante", src: file2 },
+  { title: "Torero", singer: "Chayanne", src: file3 },
+  { title: "Ave María", singer: "David Bisbal", src: file4 },
+  { title: "Vivo por ella", singer: "Andrea Bocelli", src: file5 },
+  { title: "Que la detengan", singer: "David Civera", src: file6 },
+  { title: "La madre de Jose", singer: "El Canto del Loco", src: file7 },
+  { title: "Dragostea Din Tei", singer: "O-Zone", src: file8 },
+  { title: "Como Camarón", singer: "Estopa", src: file9 },
+  { title: "Coti x coti", singer: "Mon DJ, DJ Capde (The Tyets)", src: file10 },
+  { title: "Una vaina loca", singer: "Fuego", src: file11 },
+  { title: "La canción del velero", singer: "La fiesta", src: file12 },
+  { title: "Antes muerta que sencilla", singer: "María Isabel", src: file13 },
+  { src: file14, title: "Lavantando las manos", singer: "" },
+  { src: file15, title: "entre poetas y presos", singer: "" },
+  { src: file16, title: "Despacito", singer: "" },
+  { src: file17, title: "Caminando por la vida", singer: "" },
+  { src: file18, title: "Bailando por ahi", singer: "" },
+  { src: file19, title: "A tu lado", singer: "OT" },
+  { src: file20, title: "Mienteme", singer: "" },
+  { src: file21, title: "Un beso y una flor", singer: "" },
+  { src: file22, title: "Como te atreves a volver", singer: "" },
+  { src: file23, title: "Ni una sola palabra", singer: "" },
+  { src: file24, title: "Princesas", singer: "" },
+  { src: file25, title: "Nada fue un error", singer: "" },
+  { src: file26, title: "Mi gran noche", singer: "" },
+  { src: file27, title: "Mon amour", singer: "" },
+  { src: file28, title: "Sofia", singer: "" },
+  { src: file29, title: "Fiesta Pagana", singer: "" },
+  { src: file30, title: "Sube la mano parriba", singer: "" },
+  { src: file31, title: "Noche de estrellas", singer: "" },
+  { src: file32, title: "Suerte (wherever whererver)", singer: "Shakira" },
+  { src: file33, title: "Highway to hell", singer: "" },
+  { src: file34, title: "Barbie Girl", singer: "" },
+  { src: file35, title: "It's My life", singer: "" },
+  { src: file36, title: "video killed the radio star", singer: "" },
+  { src: file37, title: "basket case", singer: "" },
+  { src: file38, title: "Don't Stop me now", singer: "" },
+  { src: file39, title: "Wannabe", singer: "" },
+  { src: file40, title: "Ni tu ni nadie", singer: "" },
+  { src: file41, title: "It's Raining men", singer: "" },
+  { src: file42, title: "Loco", singer: "" },
+  { src: file43, title: "Let me out", singer: "" },
+  { src: file44, title: "sweet caaroline", singer: "" },
+  { src: file45, title: "Sueño su boca", singer: "" },
+  { src: file46, title: "You_re the one", singer: "" },
+  { src: file47, title: "Libertad Nil Moliner", singer: "" },
+  { src: file48, title: "Voglio vederti danzare", singer: "" },
+  { src: file49, title: "Corazón latino", singer: "" },
+  { src: file50, title: "Física y química", singer: "" },
+  { src: file51, title: "Una foto en blanco y negro", singer: "" },
+  { src: file52, title: "Viva la vida", singer: "Coldplay" },
+  { src: file53, title: "La reina del pop", singer: "" },
+  { src: file54, title: "Flying Free", singer: "" },
+  { src: file55, title: "King George", singer: "" },
+  { src: file56, title: "Dime", singer: "" },
+  { src: file57, title: "Europe's Living a celebraton", singer: "" },
+  { src: file58, title: "Cuando Zarpa el amor", singer: "" },
+  { src: file59, title: "Yo quiero bailar", singer: "" },
+  { src: file60, title: "Me muero por conocerte", singer: "" },
+  { src: file61, title: "", singer: "" },
+  { src: file62, title: "", singer: "" },
+  { src: file63, title: "", singer: "" },
+  { src: file64, title: "", singer: "" },
+  { src: file65, title: "", singer: "" },
+  { src: file66, title: "", singer: "" },
+  { src: file67, title: "", singer: "" },
+  { src: file68, title: "", singer: "" },
+  { src: file69, title: "", singer: "" },
+  { src: file70, title: "", singer: "" },
+  { src: file71, title: "", singer: "" },
+  { src: file72, title: "", singer: "" },
+  { src: file73, title: "", singer: "" },
+  { src: file74, title: "", singer: "" },
+  { src: file75, title: "", singer: "" },
+  { src: file76, title: "", singer: "" },
+  { src: file77, title: "", singer: "" },
+  { src: file78, title: "", singer: "" },
+  { src: file79, title: "", singer: "" },
+  { src: file80, title: "", singer: "" },
+  { src: file81, title: "", singer: "" },
+  { src: file82, title: "", singer: "" },
+  { src: file83, title: "", singer: "" },
+  { src: file84, title: "", singer: "" },
+  { src: file85, title: "", singer: "" },
+  { src: file86, title: "", singer: "" },
+  { src: file87, title: "", singer: "" },
+  { src: file88, title: "", singer: "" },
+  { src: file89, title: "", singer: "" },
+  { src: file90, title: "", singer: "" },
 ];
