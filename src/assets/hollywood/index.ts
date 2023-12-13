a6c31af50ebc3a342bf84db333e4da092279bed3
gidina <georginaserraoliveras@gmail.com>
2023-12-13 21:18:19 +0100

    Add files via upload

---
+++ src/assets/hollywood/index.ts
@@ -0,0 +1,143 @@
+import file1 from "./1-b-s-o-el-rey-leon-el-ciclo-de-la-vida_ezk9hQwx.mp3";
+import file2 from "./2.Hakuna matata.mp3";
+import file3 from "./3-friends-opening-season-1_KOfpRcMr.mp3";
+import file4 from "./4-alla-tu_EmiKOLX2.mp3";
+import file5 from "./5-juego-de-tronos-tema_dkHxl8Y6.mp3";
+import file6 from "./6-senor-de-los-anillos-la-comunidad-del-anillo_ZIcrao2x.mp3";
+import file7 from "./7mulan-con-valor-cancion-completa-hd-128-kbps_zv4brc38.mp3";
+import file8 from "./8hijo-de-hombre-5k8nysrpply_L1W5IL0X.mp3";
+import file9 from "./9la-sirenita-bajo-el-mar-4k-castellano-fihgqmblzj8_4dc9gPM5.mp3";
+import file10 from "./10-doraemon-opening-1-catala-q-y12zumse_2CKPkaZP.mp3";
+import file11 from "./11shin-chan-dance-3xl-dance-cnqhkitjyqc_Au0li91l.mp3";
+import file12 from "./12-hattori-el-ninja-catala_CkRJrozH.mp3";
+import file13 from "./13abuelito-dime-tu_xaW5jJam.mp3";
+import file14 from "./14-102-opening-1-catala-qubuzsevewc_QXtxZfT2.mp3";
+import file15 from "./15-dartacan-y-los-tres-mosqueperros-intro-0brjnrodhzk_COwDzTLi.mp3";
+import file16 from "./16-star-wars_UEhgO7tN.mp3";
+import file17 from "./17-dr-slump-opening-catala-ts1j4lgexcg_v4wZDRfY.mp3";
+import file18 from "./18-musculman_mbIxDStZ.mp3";
+import file19 from "./19grand-prix-1999-hq_Q1uddg0c.mp3";
+import file20 from "./20-pasion-de-gavilanes-quien-es-ese-hombrelyrics_8R1yizOR.mp3";
+import file21 from "./21-sylvan-opening-metal-cover-en-catala-wyzzg1j1xxo_CrxRcKz5.mp3";
+import file22 from "./22soc-el-rovello-remix-pgck1r2igaw_xhmDSOCl.mp3";
+import file23 from "./23-els-barrufets-tv3_IfyGvqCE.mp3";
+import file24 from "./24-pretty-woman-subespanol-tfdoshtj4re_xovudICy.mp3";
+import file25 from "./25-dragon-ball-opening-catala_RfRUZfGd.mp3";
+import file26 from "./26oliver-y-benji-campeones-cancion-completa-espanol_UCWNsKjz.mp3";
+import file27 from "./27-operacion-triunfo-2020-ot-2020-sintonia-oficial-completa_Z0c7tu31.mp3";
+import file28 from "./28salvame-telecinco-2011-2013-hd-7da4odowu1w_Oj8G8mAk.mp3";
+import file29 from "./29-amar-es-para-siempre-con-bustamante_unRtdtmP.mp3";
+import file30 from "./30peter-pan-aquella-estrella-de-allaflv-gwfswhqveny_hvgPNmma.mp3";
+import file31 from "./31-let-it-go-from-frozen-audio-fnpjbkamk44_QkgMgr6z.mp3";
+import file32 from "./32-el-libro-de-la-selva-busca-lo-mas-vital_tCJk5kD0.mp3";
+import file33 from "./33quiero-ser-como-tu-latino-video-letra-68spodedzfg_7jY04uvM.mp3";
+import file34 from "./34toy-story-hay-un-amigo-en-mi-tony-cruz-con-letra-bso-original-mwt-s_e0Ecsx0b.mp3";
+import file35 from "./35-cinderella-2015-bibbidi-bobbidi-boo-helena-bonham-carter-by-patrick-do_4bc9QO6b.mp3";
+import file36 from "./36colores-en-el-viento-con-letra-pocahontas-tf42lsknsqs_Sqz3f9wr.mp3";
+import file37 from "./37aladdin-un-mundo-ideal-1080p-castellano_jOi6sIf0.mp3";
+import file38 from "./38la-bella-y-la-bestia-espana-la-bella-y-la-bestia-xykawcd0tkq_AIeg5eWH.mp3";
+import file39 from "./39no-importa-la-distancia-ricky-martin-official-video-musical-gatdntakiww-petvk4_fvzmxaGS.mp3";
+import file40 from "./40suena-el-jorobado-de-notre-dame-luis-miguel-ltmy6froylm_ARNAhn4e.mp3";
+import file41 from "./41coco-un-poco-loco-letra-lyrics-m9mraeudxnk_ZjBbSxQM.mp3";
+import file42 from "./42la-pantera-rosa-intro-mtkubgmhlkw_ymWDdSFn.mp3";
+import file43 from "./43phineas-y-ferb-hd-castellano-yfwdntchapo_v4HA2pDI.mp3";
+import file44 from "./44recreo-intro-01i4nhr74se_6WFn1pvU.mp3";
+import file45 from "./45-youre-the-one-that-i-want-grease-lyrics_WEzTDPgN.mp3";
+import file46 from "./46los-chicos-del-coro-caresse-sur-l-ocean-yun1s4vlqhg_Y1CPJPrO.mp3";
+import file47 from "./47adams_lNLWMjPo.mp3";
+import file48 from "./48.The Big Bang Theory _ Season 1 _ Opening - Intro HD_6CawmYcysAQ.mp3";
+import file49 from "./49crims-tv3-wfbczolyg0s_8jmJb11N.mp3";
+import file50 from "./50-titanic_TzenYFmo.mp3";
+import file51 from "./47adams_lNLWMjPo.mp3";
+import file52 from "./47adams_lNLWMjPo.mp3";
+import file53 from "./47adams_lNLWMjPo.mp3";
+import file54 from "./47adams_lNLWMjPo.mp3";
+import file55 from "./47adams_lNLWMjPo.mp3";
+import file56 from "./47adams_lNLWMjPo.mp3";
+import file57 from "./47adams_lNLWMjPo.mp3";
+import file58 from "./47adams_lNLWMjPo.mp3";
+import file59 from "./47adams_lNLWMjPo.mp3";
+import file60 from "./47adams_lNLWMjPo.mp3";
+import file61 from "./47adams_lNLWMjPo.mp3";
+import file62 from "./47adams_lNLWMjPo.mp3";
+import file63 from "./47adams_lNLWMjPo.mp3";
+import file64 from "./47adams_lNLWMjPo.mp3";
+import file65 from "./47adams_lNLWMjPo.mp3";
+import file66 from "./47adams_lNLWMjPo.mp3";
+import file67 from "./47adams_lNLWMjPo.mp3";
+import file68 from "./47adams_lNLWMjPo.mp3";
+import file69 from "./47adams_lNLWMjPo.mp3";
+import file70 from "./47adams_lNLWMjPo.mp3";
+
+export default [
+  { src: file1, title: "El ciclo de la vida", singer: "El rey leon" },
+  { src: file2, title: "Hakuna matata", singer: "El rey leon" },
+  { src: file3, title: "Opening 1", singer: "Friends" },
+  { src: file4, title: "", singer: "" },
+  { src: file5, title: "", singer: "Juego de tronos" },
+  { src: file6, title: "", singer: "El señor de los anillos" },
+  { src: file7, title: "Con valor", singer: "Mulan" },
+  { src: file8, title: "Hijo de hombre", singer: "Tarzan" },
+  { src: file9, title: "Bajo del mar", singer: "La Sirenita" },
+  { src: file10, title: "Opening 1", singer: "Doraemon" },
+  { src: file11, title: "", singer: "Shin-chan" },
+  { src: file12, title: "", singer: "Hattori el Ninja" },
+  { src: file13, title: "Abuelito dime tú", singer: "Heidi" },
+  { src: file14, title: "Opening 1", singer: "10+2" },
+  { src: file15, title: "", singer: "D'artacan y los tres mosqueperros" },
+  { src: file16, title: "The story continues...", singer: "Star Wars" },
+  { src: file17, title: "Opening", singer: "Dr Slump" },
+  { src: file18, title: "", singer: "Musculman" },
+  { src: file19, title: "", singer: "Grand prix" },
+  { src: file20, title: "Quien es ese hombre", singer: "Pasión de Gavilanes" },
+  { src: file21, title: "Opening", singer: "Sylvan" },
+  { src: file22, title: "Sóc el Rovelló", singer: "Rovelló" },
+  { src: file23, title: "", singer: "Els barrufets" },
+  { src: file24, title: "", singer: "Pretty woman" },
+  { src: file25, title: "Opening", singer: "Bola de drac" },
+  { src: file26, title: "Campeones", singer: "Oliver y Benji" },
+  { src: file27, title: "", singer: "OT" },
+  { src: file28, title: "Opening", singer: "Sálvame" },
+  { src: file29, title: "", singer: "Amar es para siempre" },
+  { src: file30, title: "Aquella estrella de allá", singer: "Peter Pan" },
+  { src: file31, title: "Let it go", singer: "Frozen" },
+  { src: file32, title: "Busca lo más vital", singer: "El libro de la selva" },
+  { src: file33, title: "Quiero ser como tú", singer: "El libro de la selva" },
+  { src: file34, title: "Hay un amigo en mi", singer: "Toy story" },
+  { src: file35, title: "Bibbidi Bobbidi Boo", singer: "Cinderella" },
+  { src: file36, title: "Colores en el viento", singer: "Pocahontas" },
+  { src: file37, title: "Un mundo ideal", singer: "Aladdin" },
+  { src: file38, title: "Bella y bestia son", singer: "La bella y la bestia" },
+  { src: file39, title: "No importa la distancia", singer: "Hèrcules" },
+  { src: file40, title: "Sueña", singer: "El jorobado de Notre Dame" },
+  { src: file41, title: "Un poco loco", singer: "Coco" },
+  { src: file42, title: "Opening", singer: "La pantera rosa" },
+  { src: file43, title: "Opening", singer: "Phineas y Ferb" },
+  { src: file44, title: "Recreo", singer: "La banda del patrio" },
+  { src: file45, title: "You're the one that I want", singer: "Grease" },
+  { src: file46, title: "Caresse sur l'océan", singer: "Les choristes" },
+  { src: file47, title: "Opening", singer: "La família Addams" },
+  { src: file48, title: "Opening 1", singer: "The Big Bang" },
+  { src: file49, title: "", singer: "Crims" },
+  { src: file50, title: "My heart will go on", singer: "Titanic" },
+  { src: file51, title: "", singer: "" },
+  { src: file52, title: "", singer: "" },
+  { src: file53, title: "", singer: "" },
+  { src: file54, title: "", singer: "" },
+  { src: file55, title: "", singer: "" },
+  { src: file56, title: "", singer: "" },
+  { src: file57, title: "", singer: "" },
+  { src: file58, title: "", singer: "" },
+  { src: file59, title: "", singer: "" },
+  { src: file60, title: "", singer: "" },
+  { src: file61, title: "", singer: "" },
+  { src: file62, title: "", singer: "" },
+  { src: file63, title: "", singer: "" },
+  { src: file64, title: "", singer: "" },
+  { src: file65, title: "", singer: "" },
+  { src: file66, title: "", singer: "" },
+  { src: file67, title: "", singer: "" },
+  { src: file68, title: "", singer: "" },
+  { src: file69, title: "", singer: "" },
+  { src: file70, title: "", singer: "" },
+];
