89d25da0224eb45a0d5d657c76e681f946adf7ee
gidina <georginaserraoliveras@gmail.com>
2023-12-24 19:08:06 +0100

    Update Cantada.tsx

--- src/pages/Cantada.tsx
+++ src/pages/Cantada.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import BingoDobleMusical from "../BingoDobleMusical";
 import { PiMicrophoneStageFill } from "react-icons/pi";
+import Bingo from "../components/Bingo";
+import QuinaDetailTitle from "../components/QuinaDetailTitle";
 
 const Cantada = () => {
-  return     <div>
-  <h2 className="absolute right-16 top-36 text-4xl font-semibold flex gap-4">
-    Quina Cantada <PiMicrophoneStageFill />
-  </h2>
-  <BingoDobleMusical />
-</div>;
-}
+  return (
+    <div className="w-full h-full flex m-auto">
+      <QuinaDetailTitle>
+        Quina Cantada <PiMicrophoneStageFill />
+      </QuinaDetailTitle>
+      <Bingo awards={["Línia", "Quina", "Quina"]} />
+    </div>
+  );
+};
 
 export default Cantada;
