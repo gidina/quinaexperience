3f4cdfa0077e4921ab2059686745f88d7c06201d
gidina <georginaserraoliveras@gmail.com>
2023-12-24 19:09:45 +0100

    Update Tongo.tsx

--- src/pages/Tongo.tsx
+++ src/pages/Tongo.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import BingoDobleMusical from "../BingoDobleMusical";
-import { ImEvil2 } from "react-icons/im";
 import { Link } from "react-router-dom";
+import { ImEvil2 } from "react-icons/im";
+import Bingo from "../components/Bingo";
+import QuinaDetailTitle from "../components/QuinaDetailTitle";
 
 const Tongo = () => {
-  return     <div>
-  <h2 className="absolute right-16 top-36 text-4xl font-semibold flex gap-4">
-    Quina Tongo <ImEvil2 />
-    <Link to="/stream" className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Ruleta</Link>
-  </h2>
-  <BingoDobleMusical isManualMode />
-</div>;
-}
+  return (
+    <div className="w-full h-full flex m-auto">
+      <QuinaDetailTitle>
+        Quina Tongo <ImEvil2 />
+        <Link
+          to="/stream"
+          className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+        >
+          Ruleta
+        </Link>
+      </QuinaDetailTitle>
+      <Bingo isManualMode awards={["Línia", "Quina"]} />
+    </div>
+  );
+};
 
 export default Tongo;
