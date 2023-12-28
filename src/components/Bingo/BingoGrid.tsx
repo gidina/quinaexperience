684e645fb311484675d5734244e98865247c400e
gidina <georginaserraoliveras@gmail.com>
2023-12-28 01:19:46 +0100

    Update BingoGrid.tsx

--- src/components/Bingo/BingoGrid.tsx
+++ src/components/Bingo/BingoGrid.tsx
@@ -19,7 +19,7 @@ const BingoItem = ({ text, drawed }: BingoItem) => {
       // className={`bg-[antiquewhite] text-4xl py-10 px-4 rounded-xl ${
       //   drawed ? "bg-green-400" : ""
       // }`}
-      className={`bg-[antiquewhite] text-3xl rounded-xl aspect-square flex justify-center items-center ${
+      className={`bg-[antiquewhite] text-xl md:text-3xl lg:text-5xl border border-black aspect-square rounded-lg flex justify-center items-center ${
         drawed ? "bg-[color:var(--color-tongo)]" : ""
       }`}
     >
@@ -54,7 +54,7 @@ const BingoGrid = ({ drawedNumbers, maxNumber }: BingoGrid) => {
 
   return (
     // <div className="grid grid-cols-[repeat(10,25px)] grid-rows-[25px] gap-4 text-center items-center justify-center">
-    <div className="h-full w-full grid grid-cols-10 auto-rows-min gap-4 text-center items-center justify-center">
+    <div className="h-full grid grid-cols-[repeat(10,8vh)] grid-rows-[repeat(9,8vh)] gap-2.5 text-center items-center justify-center overflow-hidden">
     {/* // <div className="grid grid-cols-10 grid-rows-10 gap-4 text-center items-center justify-center w-full"> */}
       {numberIndexes.map((number, index) => (
         <BingoItem
