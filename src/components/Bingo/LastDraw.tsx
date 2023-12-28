929826ddb586c7533601e9d06bd582a9a11c367c
gidina <georginaserraoliveras@gmail.com>
2023-12-28 01:22:14 +0100

    Update LastDraw.tsx

--- src/components/Bingo/LastDraw.tsx
+++ src/components/Bingo/LastDraw.tsx
@@ -10,12 +10,10 @@ interface LastDraw {
 const LastDraw = ({ drawedNumbers }: LastDraw) => {
   const lastNumber = drawedNumbers[drawedNumbers.length - 1];
 
-  // TODO: dont use absolute position for last label (Últim)
   return (
-    <div className="w-full flex flex-col gap-2 justify-center items-center bg-[#FFBB5C] p-16 rounded-xl h-44 text-center">
-      <div className="text-md font-light uppercase">Últim</div>
-      <div className="text-6xl font-bold">{lastNumber || "-"}</div>
-      <div className="text-3xl">{namesNumbers[lastNumber - 1]}</div>
+    <div className="w-full flex flex-col gap-2 justify-center items-center bg-[#FFBB5C] px-16 py-4 rounded-xl text-center">
+      <div className="text-3xl md:text-6xl lg:text-9xl font-bold">{lastNumber || "-"}</div>
+      <div className="text-xl md:text-3xl">{namesNumbers[lastNumber - 1] || "-"}</div>
     </div>
   );
 
