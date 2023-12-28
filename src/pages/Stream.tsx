03dd97ef5f4dbbfc4985a1a913395308511afa57
gidina <georginaserraoliveras@gmail.com>
2023-12-28 17:47:24 +0100

    Update Stream.tsx

--- src/pages/Stream.tsx
+++ src/pages/Stream.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Stream = () => {
+  const [cameras, setCameras] = useState<any[]>([]);
+  const [selectedCamera, setSelectedCamera] = useState();
+
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -11,16 +14,50 @@ const Stream = () => {
     }
 
     navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(function (stream) {
-        videoRef.current.srcObject = stream;
-      })
-      .catch(function (error) {
-        console.log("Something went wrong!");
-      });
-  });
+    .enumerateDevices()
+    .then((devices) => {
+      const videoDevices = devices.filter(device => device.kind === "videoinput");
+
+      setCameras(videoDevices);
+
+      var constraints = {
+        deviceId: selectedCamera || videoDevices[0]?.deviceId
+        // advanced: { deviceId: selectedCamera }
+        // width: { min: 1024, ideal: 1280, max: 1920 },
+        // height: { min: 776, ideal: 720, max: 1080 },
+        // deviceId: { exact: selectedCamera },
+      };
+      return navigator.mediaDevices.getUserMedia({ video: constraints });
+    })
+    .then((stream) => {
+      videoRef.current.srcObject = stream;
+    })
+    .catch((err) => {
+      console.error(`${err.name}: ${err.message}`);
+    });
+  }, [selectedCamera]);
+
+  // useEffect(() => {
+  //   if (!canvasRef.current) {
+  //     return;
+  //   }
+
+  //   const canvasWidth = canvasRef.current.width;
+  //   const canvasHeight = canvasRef.current.height;
+
+  //   const context = canvasRef.current.getContext("2d");
+
+  //   window.addEventListener("resize", resizeCanvas, false);
+  //   const resizeCanvas = () => {
+  //     canvasRef.current.width = window.innerWidth;
+  //     canvasRef.current.height = window.innerHeight;
+  //     drawCanvas();
+  //   }
+  //   resizeCanvas();
+  // }, []);
 
   const drawCanvas = (img) => {
+    console.log("drawCanvas");
     if (!canvasRef.current) {
       return;
     }
@@ -29,7 +66,6 @@ const Stream = () => {
     const canvasHeight = canvasRef.current.height;
 
     const context = canvasRef.current.getContext("2d");
-
     canvasRef.current.width = getComputedStyle(canvasRef.current).width.split(
       "px"
     )[0];
@@ -41,16 +77,16 @@ const Stream = () => {
     let y = (canvasHeight - img.height * ratio) / 2;
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     context.drawImage(
-        img,
-        0,
-        0,
-        img.width,
-        img.height,
-        x,
-        y,
-        img.width * ratio,
-        img.height * ratio
-      );
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      x,
+      y,
+      img.width * ratio,
+      img.height * ratio
+    );
   };
 
   const onTakePhotoButtonClick = () => {
@@ -101,17 +137,40 @@ const Stream = () => {
 
     videoRef.current.srcObject = null;
   };
-
+  // height: calc(100vh - 80px);
+  // h-[calc(100vh-2rem)]
   return (
     <main className="p-16">
+      <select onChange={(e) => setSelectedCamera(e.target.value)} value={selectedCamera}>
+        {cameras.map(camera => <option key={camera.deviceId} value={camera.deviceId}>{camera.label}</option>)}
+      </select>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
-          <button onClick={onTakePhotoButtonClick} className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Captura</button>
-          <button onClick={stop} className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Atura</button>
-          <Link to="/" className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Ves a la Pàgina principal</Link>
+          <button
+            onClick={onTakePhotoButtonClick}
+            className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+          >
+            Captura
+          </button>
+          <button
+            onClick={stop}
+            className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+          >
+            Atura
+          </button>
+          <Link
+            to="/"
+            className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+          >
+            Ves a la Pàgina principal
+          </Link>
         </div>
         <div className="flex gap-4">
-          <video autoPlay ref={videoRef} className="h-[calc(100vh-200px)]"></video>
+          <video
+            autoPlay
+            ref={videoRef}
+            className="h-[calc(100vh-200px)]"
+          ></video>
           <canvas ref={canvasRef}></canvas>
         </div>
       </div>
