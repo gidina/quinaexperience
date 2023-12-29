1c8764c86e8f270fd88593d31402001903989546
gidina <georginaserraoliveras@gmail.com>
2023-12-29 13:27:05 +0100

    Update Tongo.tsx

--- src/pages/Tongo.tsx
+++ src/pages/Tongo.tsx
@@ -6,18 +6,18 @@ import QuinaDetailTitle from "../components/QuinaDetailTitle";
 
 const Tongo = () => {
   return (
-    <>
+    <div className="w-full h-full flex m-auto">
       <QuinaDetailTitle>
         Quina Tongo <ImEvil2 />
         <Link
-          to="/stream"
+          to="/stream/tongo"
           className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
         >
           Ruleta
         </Link>
       </QuinaDetailTitle>
       <Bingo isManualMode awards={["Línia", "Quina"]} />
-    </>
+    </div>
   );
 };
 
